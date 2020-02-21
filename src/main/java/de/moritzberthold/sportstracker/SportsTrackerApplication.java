e11046bec2395bf01010e512e7aa3828e76e4ea0
Moritz Berthold <moritz.berthold.mb@gmail.com>
2020-02-21 18:34:47 +0100

    Initial commit
    Signed-off-by: MoritzBerthold <moritz.berthold.mb@gmail.com>

---
+++ src/main/java/de/moritzberthold/sportstracker/SportsTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.moritzberthold.sportstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/moritzberthold/sportstracker/SportsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.moritzberthold.sportstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
